10fdb63aa47d630d76d577b475e77dd36cc367ec
JhordyMora <jhordy.mora@web.de>
2022-09-24 03:53:15 +0200

    modifications

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,7 +1,16 @@
+let ataqueJugador = 
+
 function iniciarJuego(){
     let botonMascotaJugador = document.querySelector("#btn-mascota");
     //console.log(botonMascotaJugador);
     botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
+
+    let botonFuego = document.querySelector("#btn-fuego");
+    botonFuego.addEventListener("click", ataqueFuego);
+    let botonAgua = document.querySelector("#btn-agua");
+    botonAgua.addEventListener("click", ataqueAgua);
+    let botonTierra = document.querySelector("#btn-tierra");
+    botonTierra.addEventListener("click", ataqueTierra);
 }
 
 function seleccionarMascotaJugador(){
@@ -12,29 +21,36 @@ function seleccionarMascotaJugador(){
     let inputTucapalma = document.querySelector("#tucapalma");
     let inputPydos = document.querySelector("#pydos");
     let spanMascotaJugador = document.querySelector("#mascota-jugador");
-    let spanMascotaEnemigo = document.querySelector("#mascota-enemigo");
 
     if(inputHipodoge.checked){
         alert("Has seleccionado a hipodoge");
         spanMascotaJugador.innerHTML = "Hipodoge"
+        //seleccionarMascotaEnemigo();
     } else if(inputCapipego.checked){
         alert("Has seleccionado a capipego");
         spanMascotaJugador.innerHTML = "Capipego"
+        //seleccionarMascotaEnemigo();
     } else if(inputRatigueya.checked){
         alert("Has seleccionado a ratigueya");
         spanMascotaJugador.innerHTML = "Ratigueya"
+        //seleccionarMascotaEnemigo();
     } else if(inputLangostelvis.checked){
         alert("Has seleccionado a langostelvis");
         spanMascotaJugador.innerHTML = "Langostelvis"
+        //seleccionarMascotaEnemigo();
     } else if(inputTucapalma.checked){
         alert("Has seleccionado a tucapalma");
         spanMascotaJugador.innerHTML = "Tucapalma"
+        //seleccionarMascotaEnemigo();
     } else if(inputPydos.checked){
         alert("Has seleccionado a pydos");
         spanMascotaJugador.innerHTML = "Pydos"
+        //seleccionarMascotaEnemigo();
     } else {
         alert("No has seleccionado ningun Mokepon");
     }
+
+    seleccionarMascotaEnemigo();
     /*
     alert("Has seleccionado una mascota");
     let Element = document.querySelector("#hipodoge");
@@ -46,6 +62,40 @@ function seleccionarMascotaJugador(){
 
 }
 
+function seleccionarMascotaEnemigo(){
+    let spanMascotaEnemigo = document.querySelector("#mascota-enemigo");
+    let jsonMokemon = {
+        1: "Hipodoge",
+        2: "Capipego",
+        3: "Ratigueya",
+        4: "Langostelvis",
+        5: "Tucapalma",
+        6: "Pydos",
+    }
+    let randomNumber = aleatorio(1,6);
 
+    spanMascotaEnemigo.innerHTML = jsonMokemon[randomNumber];
+    alert("Tu enemigo has seleccionado a " + jsonMokemon[randomNumber]);
+}
+
+function aleatorio(min, max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function ataqueFuego(){
+    ataqueJugador = "Fuego";
+    alert(ataqueJugador);
+}
+
+function ataqueAgua(){
+    ataqueJugador = "Agua";
+    alert(ataqueJugador);
+}
+
+function ataqueTierra(){
+    ataqueJugador = "Tierra";
+    alert(ataqueJugador);
+
+}
 
 window.addEventListener("load", iniciarJuego);
